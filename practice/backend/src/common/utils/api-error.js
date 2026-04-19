bc6c88017ea75a53322e9cebd1fcc49986daf34b
Santosh Bhardwaj <bhardwajsantosh148@gmail.com>
2026-04-19 18:49:34 +0530

    add api-error utility

---
+++ practice/backend/src/common/utils/api-error.js
@@ -0,0 +1,28 @@
+class ApiError extends Error {
+    constructor(statuscode, message) {
+        super(message);
+        this.statuscode = statuscode;
+    }
+
+    static conflict(message = "Success") {
+        return new ApiError(409, message);
+    }
+
+    static notFound(message = "Not Found") {
+        return new ApiError(404, message);
+    }
+
+    static forbidden(message = "Forbidden") {
+        return new ApiError(412, message);
+    }
+
+    static badRequest(message = "Bad request") {
+        return new ApiError(400, message);
+    }
+
+    static unauthorized(message = "Unauthorized") {
+        return new ApiError(401, message);
+    }
+}
+
+export default ApiError;
