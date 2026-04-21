d783d262a0df711ed556e5594310cb2e248f4418
Santosh Bhardwaj <bhardwajsantosh148@gmail.com>
2026-04-21 19:23:50 +0530

    add jwt tokens methods

---
+++ practice/backend/src/common/utils/jwt.utils.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken"
+import crypto from "crypto";
+
+const generateAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: process.env.JWT_ACCESS_EXPIRES || "15m"});
+}
+
+const verifyAccessToken = (token) => {
+    return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+}
+
+const generateRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: process.env.JWT_REFRESH_EXPIRES || "7d"});
+}
+
+const verifyRefreshToken = (token) => {
+    return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+}
+
+const generateResetToken = () => {
+    const rawToken = crypto.randomBytes(32).toString("hex");
+    const hashedToken = crypto
+    .createHash("sha256")
+    .update(rawToken)
+    .digest("hex")
+
+    return { rawToken, hashedToken };
+}
+
+export {
+    generateAccessToken,
+    generateRefreshToken,
+    generateResetToken,
+    verifyAccessToken,
+    verifyRefreshToken
+}
